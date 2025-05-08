302e9b81e74ee8ad117d9f9fa0d148c8e2b4cf14
gilang <156637129+gilang1223@users.noreply.github.com>
2025-05-08 19:32:47 +0700

    mengisi models

--- PMB-API/Models/Registration.cs
+++ PMB-API/Models/Registration.cs
@@ -2,7 +2,21 @@
 
 public class Registration
 {
-	public Registration()
-	{
-	}
+    public string name { get; set; }
+    public string email { get; set; }
+    public string phone { get; set; }
+    public string address { get; set; }
+    public string birthdate { get; set; }
+    public string password { get; set; }
+    
+    publlic Registration(string name, string email, string phone, string address, string birthdate, string password)
+    {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.birthdate = birthdate;
+        this.password = password;
+    }
+
 }
